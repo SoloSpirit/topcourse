502278bc815ef2fddcebe9ac8736f523993762bc
Sergei Simonov <sergey.simonov@myoffice.team>
2021-12-27 21:52:28 +0300

    Update 1 h (n)

--- js/accordion.js
+++ js/accordion.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
     accordions.forEach((accordion) => {
         accordion.addEventListener('click', () => {
             accordion.classList.toggle('g_tva_accordion-active');
-            console.log(accordion.classList);
         });
     })
 });
